4b2431826d0662cd95915604bc032bd39be031c8
rmungar <mgraul@iesrafaelalberti.es>
2025-02-21 11:03:38 +0100

    WAR

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
 	kotlin("jvm") version "1.9.25"
 	kotlin("plugin.spring") version "1.9.25"
+	war
 	id("org.springframework.boot") version "3.4.2"
 	id("io.spring.dependency-management") version "1.1.7"
 }
